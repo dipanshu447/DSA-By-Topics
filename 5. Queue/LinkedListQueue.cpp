3265f78113e7e7b2fb8e08d914155bce45395fea
dipanshu447 <dipanshusahu447@gmail.com>
2024-10-09 13:39:10 +0530

    Added Linked list queue file

--- 5. Queue/LinkedListQueue.cpp
+++ 5. Queue/LinkedListQueue.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class Node {
+    public:
+        int data;
+        Node *front;
+        Node *rear;
+
+        Node(int val) : data(val), front(nullptr), rear(nullptr) {}
+};
+
+class Queue{
+    private:
+        
+};
+
 int main(){
     
     
