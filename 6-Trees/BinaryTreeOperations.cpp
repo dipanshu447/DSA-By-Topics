331c9fddcb355868d9c35b6597af23c19f50b239
dipanshu447 <dipanshusahu447@gmail.com>
2024-11-19 17:11:17 +0530

    Added tree insertion & half done deletetion algo

--- 6-Trees/BinaryTreeOperations.cpp
+++ 6-Trees/BinaryTreeOperations.cpp
@@ -18,24 +18,6 @@ class Tree
 private:
     TreeNode *root;
 
-    // void pre(TreeNode *rootNode){
-    //     if (rootNode != nullptr)
-    //     {
-    //         cout << rootNode->data << " ";
-    //         pre(rootNode->left);
-    //         pre(rootNode->right);
-    //     }
-    // }
-
-public:
-    Tree() : root(nullptr) {}
-
-    TreeNode *createNode(int data)
-    {
-        TreeNode *newNode = new TreeNode(data);
-        return newNode;
-    }
-
     void pre(TreeNode *rootNode)
     {
         if (rootNode != nullptr)
@@ -45,10 +27,6 @@ public:
             pre(rootNode->right);
         }
     }
-
-    // void preOrder(){
-    //     pre(root);
-    // }
 
     void inOrd(TreeNode *rootNode)
     {
@@ -67,78 +45,6 @@ public:
             post(rootNode->left);
             post(rootNode->right);
             cout << rootNode->data << " ";
-        }
-    }
-
-    void nonrecPost(TreeNode *Rootnode)
-    {
-        stack<TreeNode *> s;          // stack will store treenode pointers
-        TreeNode *previous = nullptr; // track previous nodes
-        do                            // runs till stack is not empty
-        {
-            while (Rootnode != nullptr)
-            {
-                s.push(Rootnode);          // push that rootnode into stack to later loop through that for its other sub trees
-                Rootnode = Rootnode->left; // root goes to left subtrees
-            }
-            while (Rootnode == nullptr && !s.empty()) // processes the tree nodes
-            {
-                Rootnode = s.top();                                            // pop subtree from stack
-                if (Rootnode->right == nullptr || Rootnode->right == previous) // check if the right substree is already processed or empty
-                {
-                    cout << Rootnode->data << " "; // print node data
-                    s.pop();
-                    previous = Rootnode; // update previous
-                    Rootnode = nullptr;  // make rootnode null in order to make sure the loop runs next iterations
-                }
-                else
-                {
-                    Rootnode = Rootnode->right; // root goes to to its right subtrees
-                }
-            }
-
-        } while (!s.empty()); // continue till stack is empty
-    }
-
-    void nonrecInOrd(TreeNode *Rootnode)
-    {
-        stack<TreeNode *> s; // stack will store treenode pointers
-        while (true)
-        {
-            while (Rootnode != nullptr)
-            {
-                s.push(Rootnode);          // push that rootnode into stack to later loop through that for its other sub trees
-                Rootnode = Rootnode->left; // root goes to left subtrees
-            }
-            if (s.empty()) // if stack is empty then it means we have checked all the root subtrees so it breaks the loop
-            {
-                break;
-            }
-            Rootnode = s.top(); // pop the sub tree node to loop through
-            s.pop();
-            cout << Rootnode->data << " "; // print rootnode data
-            Rootnode = Rootnode->right;    // root goes to to its right subtrees
-        }
-    }
-
-    void nonrecPre(TreeNode *Rootnode)
-    {
-        stack<TreeNode *> s; // stack will store treenode pointers
-        while (true)
-        {
-            while (Rootnode != nullptr)
-            {
-                cout << Rootnode->data << " "; // print rootnode data
-                s.push(Rootnode);              // push that rootnode into stack to later loop through that for its other sub trees
-                Rootnode = Rootnode->left;     // root goes to left subtrees
-            }
-            if (s.empty()) // if stack is empty then it means we have checked all the root subtrees so it breaks the loop
-            {
-                break;
-            }
-            Rootnode = s.top(); // pop the sub tree node to loop through
-            s.pop();
-            Rootnode = Rootnode->right; // root goes to to its right subtrees
         }
     }
 
@@ -166,6 +72,170 @@ public:
             }
         }
     }
+
+public:
+    Tree() : root(nullptr) {}
+
+    void PreOrder()
+    {
+        pre(root);
+    }
+
+    void InOrder()
+    {
+        inOrd(root);
+    }
+
+    void PostOrder()
+    {
+        post(root);
+    }
+
+    void LevelOrder()
+    {
+        lvlOrder(root);
+    }
+
+    void Insert(int data)
+    {
+        TreeNode *newNode = new TreeNode(data);
+        TreeNode *current = root;
+        if (root == nullptr)
+        {
+            root = newNode;
+            return;
+        }
+        else
+        {
+            char choice;
+            while (true)
+            {
+                cout << "Curent Node :  " << current->data << endl;
+                cout << "New Node :  " << newNode->data << endl;
+                cout << "Enter the left(L) or right node(R) or (N) to insert here: " << endl;
+                cin >> choice;
+                if (choice == 'L' || choice == 'l')
+                {
+                    if (current->left != nullptr)
+                    {
+                        current = current->left;
+                    }
+                    else
+                    {
+                        current->left = newNode;
+                        break;
+                    }
+                }
+                else if (choice == 'r' || choice == 'R')
+                {
+                    if (current->right != nullptr)
+                    {
+                        current = current->right;
+                    }
+                    else
+                    {
+                        current->right = newNode;
+                        break;
+                    }
+                }
+                else if (choice == 'n' || choice == 'N')
+                {
+                    cout << "Insert as left(l) or right(r) child : " << endl;
+                    cin >> choice;
+                    if (choice == 'l' || choice == 'L')
+                    {
+                        if (current->left == nullptr)
+                        {
+                            current->left = newNode;
+                        }
+                        else
+                        {
+                            cout << "Left Child already exists";
+                        }
+                    }
+                    else if (choice == 'r' || choice == 'R')
+                    {
+                        if (current->right == nullptr)
+                        {
+                            current->right = newNode;
+                        }
+                        else
+                        {
+                            cout << "Right Child already exists";
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    cout << "INVALID CHOICE" << endl;
+                }
+            }
+        }
+    }
+
+    void Delection(){ // half done NOT WORKING yet It cant delete child nodes. It can only delete root node 
+        TreeNode *current = root;
+        char choice;
+        if (root == nullptr)
+        {
+            cout << "Tree is empty nothing to delete" << endl;
+            return;
+        }
+        else
+        {
+            while (true)
+            {
+                cout << "Curent Node :  " << current->data << endl;
+                cout << "Delete this node (Y/N) : " << endl;
+                cin >> choice;
+                if(choice == 'Y' || choice == 'y'){
+                    if(current == root){
+                        delete root;
+                        root = nullptr;
+                        cout << "Root node deleted" << endl;
+                        return;
+                    }
+                    else {
+                        cout << "Cannot delete this node as root" << endl;
+                    }
+                }
+
+                cout << "Move the left(L) or right node(R) or (N) to insert here: " << endl;
+                cin >> choice;
+                if (choice == 'L' || choice == 'l')
+                {
+                    if (current->left != nullptr)
+                    {
+                        current = current->left;
+                    }
+                    else
+                    {
+                        delete current->left;
+                        current->left = nullptr;
+                        break;
+                    }
+                }
+                else if (choice == 'r' || choice == 'R')
+                {
+                    if (current->right != nullptr)
+                    {
+                        current = current->right;
+                    }
+                    else
+                    {
+                        delete current->right;
+                        current->right = nullptr;
+                        break;
+                    }
+                }
+                else
+                {
+                    cout << "INVALID CHOICE" << endl;
+                }
+            }
+        }
+    }
 };
 
 int main()
@@ -178,28 +248,13 @@ int main()
     //  /
     // 40
 
-    TreeNode *n1 = t.createNode(10);
-    TreeNode *n2 = t.createNode(20);
-    TreeNode *n3 = t.createNode(30);
-    TreeNode *n4 = t.createNode(40);
+    t.Insert(10);
+    t.Insert(20);
+    t.Insert(30);
+    t.Insert(40);
 
-    n1->left = n2;
-    n1->right = n3;
-    n2->left = n4;
+    t.Delection();
 
-    // t.pre(n1);
-    // cout << endl;
-    // t.nonrecPre(n1);
-
-    // t.inOrd(n1);
-    // cout << endl;
-    // t.nonrecInOrd(n1);
-
-    // t.post(n1);
-    // cout << endl;
-    // t.nonrecPost(n1);
-
-    t.lvlOrder(n1);
 
     return 0;
 }
