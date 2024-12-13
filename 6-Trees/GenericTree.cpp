08207b64e68d7c4b206b83e7e7b316d0f6d44f87
dipanshu447 <dipanshusahu447@gmail.com>
2024-12-13 13:54:40 +0530

    Implemented preorder traversal in generic tree

--- 6-Trees/GenericTree.cpp
+++ 6-Trees/GenericTree.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 using namespace std;
 
 class TreeNode {
+public:
     int data;
-    vector <TreeNode*> children;
+    vector <TreeNode*> child;
 
     TreeNode(int val) : data(val) {}
 
-    void addChild(TreeNode * child){
-        children.push_back(child);
+    void addChild(TreeNode * newchild){
+        child.push_back(newchild);
     }
 
     TreeNode *createTree(){
@@ -30,12 +32,40 @@ class TreeNode {
         child2->addChild(child2_1);
         child2->addChild(child2_2);
         child2->addChild(child2_3);
+
+        return root;
+    }
+
+    void preorder(TreeNode* Root){
+        stack<TreeNode*> Stack;
+
+        Stack.push(Root);
+        while (!Stack.empty())
+        {
+            TreeNode *temp = Stack.top();
+            Stack.pop();
+
+            cout << temp->data << " ";
+
+            for (int i = temp->child.size()-1; i >= 0 ; i--)
+            {
+                Stack.push(temp->child[i]);
+            }
+        }
+        
     }
 };
 
 int main(){
     
-    
+    TreeNode* r = new TreeNode(0);
+    TreeNode *t = r->createTree();
+    //         1
+    //      /  |  \
+    //     2   3   4
+    //   / | \
+    //  5  6  7
+    r->preorder(t);
       
     return 0;
 }
