c537ad36e103c5a704784176ba6f455245aff633
dipanshu447 <dipanshusahu447@gmail.com>
2024-12-16 15:45:15 +0530

    working on inorder traversal of generic tree

--- 6-Trees/GenericTree.cpp
+++ 6-Trees/GenericTree.cpp
@@ -3,18 +3,21 @@
 #include <vector>
 using namespace std;
 
-class TreeNode {
+class TreeNode
+{
 public:
     int data;
-    vector <TreeNode*> child;
+    vector<TreeNode *> child;
 
     TreeNode(int val) : data(val) {}
 
-    void addChild(TreeNode * newchild){
+    void addChild(TreeNode *newchild)
+    {
         child.push_back(newchild);
     }
 
-    TreeNode *createTree(){
+    TreeNode *createTree()
+    {
         TreeNode *root = new TreeNode(1);
 
         TreeNode *child1 = new TreeNode(2);
@@ -36,8 +39,9 @@ public:
         return root;
     }
 
-    void preorder(TreeNode* Root){
-        stack<TreeNode*> Stack;
+    void preorder(TreeNode *Root)
+    {
+        stack<TreeNode *> Stack;
 
         Stack.push(Root);
         while (!Stack.empty())
@@ -47,25 +51,62 @@ public:
 
             cout << temp->data << " ";
 
-            for (int i = temp->child.size()-1; i >= 0 ; i--)
+            for (int i = temp->child.size() - 1; i >= 0; i--)
             {
                 Stack.push(temp->child[i]);
             }
         }
+    }
+
+    // void inOrder(TreeNode *Root)
+    // {
+    //     if (Root == nullptr)
+    //     {
+    //         return;
+    //     }
+
+    //     int n = Root->child.size();
+    //     for (int i = 0; i < n - 1; i++)
+    //     {
+    //         inOrder(Root->child[i]);
+    //     }
+
+    //     cout << Root->data << " ";
+    //     if (n > 0)
+    //     {
+    //         inOrder(Root->child[n - 1]);
+    //     }
+    // }
+    void inOrder(TreeNode *Root)
+    {
+        if (Root == nullptr)
+        {
+            return;
+        }
+        if (!Root->child.empty())
+        {
+            inOrder(Root->child[0]);
+        }
         
+        cout << Root->data << " ";
+        for (int i = 1; i < Root->child.size(); i++)
+        {
+            inOrder(Root->child[i]);
+        }
     }
 };
 
-int main(){
-    
-    TreeNode* r = new TreeNode(0);
+int main()
+{
+
+    TreeNode *r = new TreeNode(0);
     TreeNode *t = r->createTree();
     //         1
     //      /  |  \
     //     2   3   4
     //   / | \
     //  5  6  7
-    r->preorder(t);
-      
+    // r->preorder(t);
+    r->inOrder(t);
     return 0;
 }
